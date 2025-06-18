e1cd565f6c164aa78cbb3a1d5a7c34e8757f0793
user <user@localhost>
2025-06-18 19:18:50 +0000

    sending server

---
+++ mytcpserver/mytcpserver.cpp
@@ -0,0 +1,41 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent)
+{
+    mTcpServer = new QTcpServer(this);
+
+    connect(mTcpServer, &QTcpServer::newConnection, this, &MyTcpServer::slotNewConnection);
+
+    if(!mTcpServer->listen(QHostAddress::Any, 6000)){
+        qDebug() << "server is not started";
+    } else {
+        qDebug() << "EBATS server is started";
+    }
+}
+
+void MyTcpServer::slotNewConnection()
+{
+    mTcpSocket = mTcpServer->nextPendingConnection();
+
+    mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
+
+    connect(mTcpSocket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);
+    connect(mTcpSocket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);
+}
+
+void MyTcpServer::slotServerRead()
+{
+    while(mTcpSocket->bytesAvailable()>0)
+    {
+        QByteArray array = mTcpSocket->readAll();
+
+        mTcpSocket->write(array);
+    }
+}
+
+void MyTcpServer::slotClientDisconnected()
+{
+    mTcpSocket->close();
+}
